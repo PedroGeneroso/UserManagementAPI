b6cbb9972de1e9dd1af74275d24b8f33f743718f
PedroGeneroso <pedrogenerosovd@gmail.com>
2026-06-23 17:00:10 -0300

    Assembly

--- obj/Debug/net10.0/UserManagementAPI.AssemblyInfo.cs
+++ obj/Debug/net10.0/UserManagementAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UserManagementAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c76eab6cb9ba0c8d728bc9cf74638b92fe2745b1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1e715ca05688e0c94a28a222a3e2664d015b31a2")]
 [assembly: System.Reflection.AssemblyProductAttribute("UserManagementAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UserManagementAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
